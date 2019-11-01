0cb61d9626607e20e702d686000881e72c41ae95
blossun <dnsngu@gmail.com>
2019-11-01 23:46:24 +0900

    index.html login.html 디자인 만들기 진행중

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Javakong Cafe</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <div class="topArea">
+      <a href="login.html">LOGIN</a>
+      <a href="join.html">JOIN US</a>
+      <a href="notice.html">NOTICE</a>
+      <a href="qna.html">QnA</a>
+      <a href="review.html">REVIEW</a>
+    </div>
+    <a href="index.php" id="title">Javakong Cafe</a>
+
+  </body>
+</html>
